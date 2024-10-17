f328f14f46fe0eba1c35d39e4fc9e009798837a0
unknown <arunkhajuria0052@gmail.com>
2024-10-18 01:22:48 +0530

    asa1

--- sep/sep/settings.py
+++ sep/sep/settings.py
@@ -153,6 +153,9 @@ import os
 STATIC_URL = 'static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'build','static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
